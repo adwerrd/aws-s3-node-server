e8c8a3f7b7e0450c5778c1dd83719e3621d0dea2
lan <adwerrd@hotmail.com>
2018-11-06 20:30:17 +0800

    add domainWhiteList

--- config/config.default.js
+++ config/config.default.js
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
-module.exports = appInfo => {
-  const config = exports = {};
+module.exports = (appInfo) => {
+  const config = (exports = {})
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1540376958168_571';
+  config.keys = appInfo.name + '_1540376958168_571'
 
   // add your config here
-  config.middleware = ['gzip'];
+  config.middleware = ['gzip']
   config.gzip = {
     threshold: 1024, // 小于 1k 的响应体不压缩
   }
   // config AWS host and region
-  config.awsHost = 'http://47.93.3.45';
-  config.region = 'us-west-1';
+  config.awsHost = 'http://47.93.3.45'
+  config.region = 'us-west-1'
 
- // csrf mode
+  // csrf mode
   config.security = {
     csrf: false,
     debug: 'csrf-disable',
-    domainWhiteList: [ 'http://cwn-ss.portal.baishancloud.com:8080', 'http://127.0.0.1:8080' ]
-  };
+    domainWhiteList: ['http://cwn-ss.portal.baishancloud.com:8080', 'http://127.0.0.1:8080', 'http://47.75.196.0:8080'],
+  }
 
   config.cors = {
     credentials: true,
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
-  };
-  
-  return config;
-};
+  }
+
+  return config
+}
